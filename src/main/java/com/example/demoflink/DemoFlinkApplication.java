bb4d50ecf58f9c2c36133b10f921a588752a59b2
fernando.tomasio <!Korg3286>
2022-03-04 14:30:54 -0300

    first commit

---
+++ src/main/java/com/example/demoflink/DemoFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFlinkApplication.class, args);
+	}
+
+}
